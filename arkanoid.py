ec73295bfff6b23030d95e5365e3b0bfcd61f337
Alexander Shadrin <144260991+Sh-Shanya@users.noreply.github.com>
2023-12-27 12:26:40 +0300

    Update Start_window and add fullscreen

--- arkanoid.py
+++ arkanoid.py
@@ -5,7 +5,7 @@ from pygame.locals import *
 
 pygame.init()
 pygame.display.set_caption("Арканоид")
-screen_size = screen_width, screen_height = 800, 600
+screen_size = screen_width, screen_height = 1580, 900
 screen = pygame.display.set_mode(screen_size, pygame.SCALED | pygame.FULLSCREEN)
 
 running = True
@@ -23,8 +23,8 @@ ball_x = screen_width // 2
 ball_y = platform_y - ball_radius - 1
 ball = pygame.Rect(ball_x, ball_y, ball_radius, ball_radius)
 
-ball_speed_x = 3
-ball_speed_y = -3
+ball_speed_x = 6
+ball_speed_y = -6
 
 brick_width = 55
 brick_height = 30
@@ -66,20 +66,69 @@ def load_image(name, color_key=None):
 
 
 def start_screen():
-
-    fon = pygame.transform.scale(load_image("start_window.png"), (800, 600))
+    fon = pygame.transform.scale(load_image("start_window.png"), (1580, 900))
     screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
+
+    width = screen.get_width()
+    height = screen.get_height()
+
+    smallfont = pygame.font.SysFont("Corbel", 35)
+    color_light = (170, 170, 170)
+    color_dark = (100, 100, 100)
+
+    text_play = smallfont.render("play", True, "white")
+    text_quit = smallfont.render("quit", True, "white")
+    text_settings = smallfont.render("settings", True, "white")
+    text_guide = smallfont.render("guide", True, "white")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if width / 2 - 140 <= mouse[0] <= width / 2 and 250 <= mouse[1] <= 290:
+                    return
+                elif (
+                    width / 2 - 140 <= mouse[0] <= width / 2 and 300 <= mouse[1] <= 340
+                ):
+                    pass
+                elif (
+                    width / 2 - 140 <= mouse[0] <= width / 2 and 350 <= mouse[1] <= 390
+                ):
+                    pass
+                elif (
+                    width / 2 - 140 <= mouse[0] <= width / 2 and 400 <= mouse[1] <= 440
+                ):
+                    terminate()
+
+        mouse = pygame.mouse.get_pos()
+
+        if width / 2 - 140 <= mouse[0] <= width / 2 and 250 <= mouse[1] <= 290:
+            pygame.draw.rect(screen, color_light, [width / 2 - 140, 250, 140, 40])
+        else:
+            pygame.draw.rect(screen, color_dark, [width / 2 - 140, 250, 140, 40])
+
+        if width / 2 - 140 <= mouse[0] <= width / 2 and 300 <= mouse[1] <= 340:
+            pygame.draw.rect(screen, color_light, [width / 2 - 140, 300, 140, 40])
+        else:
+            pygame.draw.rect(screen, color_dark, [width / 2 - 140, 300, 140, 40])
+
+        if width / 2 - 140 <= mouse[0] <= width / 2 and 350 <= mouse[1] <= 390:
+            pygame.draw.rect(screen, color_light, [width / 2 - 140, 350, 140, 40])
+        else:
+            pygame.draw.rect(screen, color_dark, [width / 2 - 140, 350, 140, 40])
+
+        if width / 2 - 140 <= mouse[0] <= width / 2 and 400 <= mouse[1] <= 440:
+            pygame.draw.rect(screen, color_light, [width / 2 - 140, 400, 140, 40])
+        else:
+            pygame.draw.rect(screen, color_dark, [width / 2 - 140, 400, 140, 40])
+
+        screen.blit(text_play, (width / 2 - 100, 250))
+        screen.blit(text_settings, (width / 2 - 125, 300))
+        screen.blit(text_guide, (width / 2 - 110, 350))
+        screen.blit(text_quit, (width / 2 - 100, 400))
+
         pygame.display.flip()
-        clock.tick(60)
 
 
 start_screen()
@@ -92,15 +141,17 @@ while running:
     for brick in bricks:
         pygame.draw.rect(screen, "white", brick)
 
+    d = pygame.key.get_pressed()
+
     for event in pygame.event.get():
-        if event.type == QUIT:
+
+        if d[pygame.K_ESCAPE] or event.type == QUIT:
             running = False
 
-    d = pygame.key.get_pressed()
     if d[K_LEFT] and platform.left > 0:
-        platform.left -= 5
+        platform.left -= 10
     if d[K_RIGHT] and platform.right < screen_width:
-        platform.right += 5
+        platform.right += 10
 
     ball.x += ball_speed_x
     ball.y += ball_speed_y
